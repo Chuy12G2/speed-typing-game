366db193b5932dedf5ec9f8218bfd37f30891c29
jesus <chuygs12@gmail.com>
2023-06-01 17:53:14 -0600

    create functionality to start and end the game

--- src/App.js
+++ src/App.js
@@ -1,15 +1,52 @@
+import { useEffect, useState } from "react";
 import "./styles.css";
 
 function App() {
+  const [text, setText] = useState("")
+  const [timer, setTimer] = useState(5)
+  const [isTimeRunning, setIsTimeRunning] = useState(false)
+  const [wordCount, setWordCount] = useState(0)
+
+  function onChangeText(event) {
+    setText(event.target.value)
+  }
+
+  function calculateWordCount(text) {
+    const words = text.trim().split(" ")
+    const filteredWords = words.filter(word => word !== "")
+    return filteredWords.length
+  }
+
+  function startGame() {
+    setIsTimeRunning(true)
+    setTimer(5)
+    setText("")
+  }
+
+  function endGame() {
+    setIsTimeRunning(false)
+    setWordCount(calculateWordCount(text))
+  }
+
+  useEffect(() => {
+    if(isTimeRunning && timer > 0){
+      setTimeout(() => {
+        setTimer(prevState => prevState - 1)
+      }, 1000)
+    } else if(timer === 0){
+      endGame()
+    }
+  }, [timer, isTimeRunning])
+
   return (
     <div className="App">
       <h1>How fast can you type?</h1>
-      <textarea></textarea>
-      <h4>time remaining</h4>
-      <button>START</button>
-      <h1>Word count</h1>
+      <textarea disabled={!isTimeRunning} onChange={onChangeText} name="text" value={text}></textarea>
+      <h4>time remaining: {timer}</h4>
+      <button disabled={isTimeRunning} onClick={startGame}>START</button>
+      <h1>Word count: {wordCount}</h1>
     </div>
-  )
+  );
 }
 
 export default App;
